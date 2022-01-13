18efb66efa3f2342bbcdd8c03d81835395d9e333
ledouxm <mledoux@mledoux.fr>
2022-01-13 01:09:06 +0100

    feat: add rank difference function

--- electron/utils.ts
+++ electron/utils.ts
@@ -5,3 +5,83 @@ export const makeDebug = (suffix: string) => debug("lol-stalking").extend(suffix
 export const formatRank = (
     ranking: Pick<Prisma.RankingCreateInput, "division" | "tier" | "leaguePoints">
 ) => `${ranking.tier} ${ranking.division} - ${ranking.leaguePoints}`;
+
+export const ranks: Rank[] = [
+    {
+        tier: "IRON",
+        division: "IV",
+        leaguePoints: 0,
+    },
+    {
+        tier: "IRON",
+        division: "II",
+        leaguePoints: 50,
+    },
+    {
+        tier: "IRON",
+        division: "II",
+        leaguePoints: 30,
+    },
+    {
+        tier: "SILVER",
+        division: "III",
+        leaguePoints: 30,
+    },
+    {
+        tier: "CHALLENGER",
+        division: "I",
+        leaguePoints: 10,
+    },
+    {
+        tier: "CHALLENGER",
+        division: "I",
+        leaguePoints: 100,
+    },
+];
+type Tier =
+    | "IRON"
+    | "SILVER"
+    | "GOLD"
+    | "PLATINUM"
+    | "DIAMOND"
+    | "MASTER"
+    | "GRANDMASTER"
+    | "CHALLENGER";
+type Division = "I" | "II" | "III" | "IV";
+interface Rank {
+    tier: Tier;
+    division: Division;
+    leaguePoints: number;
+}
+const tiers = [
+    "IRON",
+    "SILVER",
+    "GOLD",
+    "PLATINUM",
+    "DIAMOND",
+    "MASTER",
+    "GRANDMASTER",
+    "CHALLENGER",
+];
+const divisions = ["IV", "III", "II", "I"];
+export const getRankDifference = (oldRank: Rank, newRank: Rank) => {
+    const sameTier = oldRank.tier === newRank.tier;
+    const sameDivision = oldRank.division === newRank.division;
+
+    const hasDivisionPromoted =
+        divisions.findIndex((division) => division === oldRank.division) <
+        divisions.findIndex((division) => division === newRank.division);
+
+    if (!sameTier || !sameDivision) {
+        const hasTierPromoted =
+            tiers.findIndex((tier) => tier === oldRank.tier) <
+            tiers.findIndex((tier) => tier === newRank.tier);
+        const hasPromoted = hasTierPromoted || (hasDivisionPromoted && sameTier);
+        return `${hasPromoted ? "PROMOTED" : "DEMOTED"} FROM ${oldRank.tier} ${
+            oldRank.division
+        } TO ${newRank.tier} ${newRank.division}`;
+    }
+
+    const lpDifference = oldRank.leaguePoints - newRank.leaguePoints;
+    return `${lpDifference > 0 ? "LOST" : "GAINED"} ${Math.abs(lpDifference)} LPs`;
+};
