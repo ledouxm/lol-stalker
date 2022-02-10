3fcf54a58b79bb9582b9236f73c70d99831e79f8
ledouxm <mledoux@mledoux.fr>
2022-02-10 16:27:17 +0100

    feat: remove all stalked summoners from discord guild

--- src/features/Discord/Discord.tsx
+++ src/features/Discord/Discord.tsx
@@ -15,7 +15,6 @@ import {
     Flex,
     Icon,
     IconButton,
-    List,
     ListItem,
     Modal,
     ModalCloseButton,
@@ -27,8 +26,7 @@ import {
 import { useSelection } from "@pastable/core";
 import { useAtomValue } from "jotai/utils";
 import { useEffect } from "react";
-import { AiOutlineArrowRight } from "react-icons/ai";
-import { BiFolder, BiLogOut, BiPlusCircle, BiRefresh, BiRightArrow } from "react-icons/bi";
+import { BiFolder, BiLogOut, BiPlusCircle, BiRefresh, BiTrash } from "react-icons/bi";
 import { FaDiscord } from "react-icons/fa";
 import { useQuery } from "react-query";
 import {
@@ -116,7 +114,7 @@ const BotInfos = (props: BoxProps) => {
                         channel you want the bot to send messages in
                     </ListItem>
                     <ListItem>
-                        <Flex alignItems="center">Add stalked summoners in the list</Flex>
+                        <Flex alignItems="center">Add stalked summoners to the list</Flex>
                     </ListItem>
                 </UnorderedList>
             </Flex>
@@ -170,19 +168,35 @@ const DiscordGuildList = (props: BoxProps) => {
                     guilds.map((guild) => (
                         <AccordionItem key={guild.guildId}>
                             <AccordionButton>
-                                <Flex direction="column" textAlign="left">
-                                    <Box fontSize="20px" pb="3px">
-                                        {guild.name} - {guild.channelName}{" "}
-                                        <chakra.span fontWeight="600">
-                                            ({guild.summoners.length})
-                                        </chakra.span>
-                                    </Box>
-                                    <Box fontSize="sm" color="gray.400" mt="-6px">
-                                        {guild.nbStalkers} active stalker
-                                        {guild.nbStalkers > 1 ? "s" : ""}
-                                    </Box>
+                                <Flex justifyContent="space-between" w="100%" alignItems="center">
+                                    <Flex direction="column" textAlign="left">
+                                        <Box fontSize="20px" pb="3px">
+                                            {guild.name} - {guild.channelName}{" "}
+                                            <chakra.span fontWeight="600">
+                                                ({guild.summoners.length})
+                                            </chakra.span>
+                                            <AccordionIcon ml="10px" />
+                                        </Box>
+                                        <Box fontSize="sm" color="gray.400" mt="-6px">
+                                            {guild.nbStalkers} active stalker
+                                            {guild.nbStalkers > 1 ? "s" : ""}
+                                        </Box>
+                                    </Flex>
+                                    <BiTrash
+                                        size="20px"
+                                        cursor="pointer"
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            window.Main.sendMessage("discord/remove-friends", {
+                                                channelId: guild.channelId,
+                                                guildId: guild.guildId,
+                                                summoners: guild.summoners.map(
+                                                    (summoner) => summoner.puuid
+                                                ),
+                                            });
+                                        }}
+                                    />
                                 </Flex>
-                                <AccordionIcon ml="10px" />
                             </AccordionButton>
                             <AccordionPanel>
                                 {guild.summoners.map((summoner) => (
