75ef78d499f6f0b661f201ce140f84fa61fe0130
ledouxm <mledoux@mledoux.fr>
2022-02-14 17:31:26 +0100

    fix: remove spectating from in game friends

--- electron/features/routes/friends.ts
+++ electron/features/routes/friends.ts
@@ -106,7 +106,9 @@ export const addOrUpdateFriends = async (friends: FriendDto[]) => {
 
     for (const friend of friends) {
         if (
-            !["outOfGame", "hosting_RANKED_SOLO_5x5"].includes(friend.lol.gameStatus) &&
+            !["outOfGame", "hosting_RANKED_SOLO_5x5", "spectating"].includes(
+                friend.lol.gameStatus
+            ) &&
             friend.lol.gameQueueType === "RANKED_SOLO_5x5"
         ) {
             currentInGame.push({
